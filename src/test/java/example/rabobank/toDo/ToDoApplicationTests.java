df21b714efcfdadfd94e351db1a76da6758367df
Eoghan <eoghan.rainey@gmail.com>
2023-07-22 21:59:20 +0200

    Initial commit

---
+++ src/test/java/example/rabobank/toDo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package example.rabobank.toDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
